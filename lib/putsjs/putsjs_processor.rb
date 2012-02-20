824ccea4014fe57bac9ccad9691f1624af1adbba
Samuel Selvanathan <samsel@vendavo.com>
2012-02-20 15:01:55 -0800

    refactored puts_js_processsor

--- lib/putsjs/putsjs_processor.rb
+++ lib/putsjs/putsjs_processor.rb
@@ -3,12 +3,8 @@ class PutsJSProcessor < Tilt::Template
   def prepare; end
 
   def evaluate(context, locals, &block)
-    modified_data = ""
-    data.lines.each do |line|
-      new_line = line.gsub!("puts", "console.log")
-      modified_data = (new_line.nil? ? (modified_data + line) : (modified_data + new_line))       
-    end
+    data.gsub!("puts", "console.log")
     
-    modified_data
+    data
   end
 end
