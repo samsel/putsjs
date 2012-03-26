10447e4724e588149e0bccf5829d69886ab015ff
Samuel Selvanathan <samsel@vendavo.com>
2012-03-25 22:42:43 -0700

    fixed unicode warning in Regex

--- lib/putsjs/putsjs_processor.rb
+++ lib/putsjs/putsjs_processor.rb
@@ -3,7 +3,7 @@ class PutsJSProcessor < Tilt::Template
   def prepare; end
 
   def evaluate(context, locals, &block)
-    data.gsub!(/\b\puts\b/, "console.log")
+    data.gsub!(/\bputs\b/, "console.log")
     
     data
   end
